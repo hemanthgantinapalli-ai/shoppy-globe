6e504777fced9605b2b513d180f068027ae17cec
ShoppyGlobe Developer <developer@shoppyglobe.com>
2026-05-13 06:32:14 +0530

    feature: persist search query across sessions

--- src/redux/searchSlice.js
+++ src/redux/searchSlice.js
@@ -1,10 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const loadSearchFromStorage = () => {
+  try {
+    const savedQuery = localStorage.getItem("shoppyglobe_search");
+    return savedQuery || "";
+  } catch (error) {
+    console.warn("Unable to load search query from storage:", error);
+    return "";
+  }
+};
+
+const saveSearchToStorage = query => {
+  try {
+    localStorage.setItem("shoppyglobe_search", query);
+  } catch (error) {
+    console.warn("Unable to save search query to storage:", error);
+  }
+};
+
 const searchSlice = createSlice({
   name: "search",
-  initialState: "",
+  initialState: loadSearchFromStorage(),
   reducers: {
-    setSearch: (state, action) => action.payload,
+    setSearch: (state, action) => {
+      saveSearchToStorage(action.payload);
+      return action.payload;
+    },
   }
 });
 
